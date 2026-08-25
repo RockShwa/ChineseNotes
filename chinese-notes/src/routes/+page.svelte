d0cd069618e3448b2b11c3fb8d5e18f7391e2108
Aubrey Tipps <aatipps@gmail.com>
2026-08-25 09:20:42 -0500

    connected svelte server to python backend!

---
+++ chinese-notes/src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import { onMount } from 'svelte';
+
+    // state declares a reactive variable with a non-computed value
+    // $derived(a + b) would declare a computed value (sum of a & b)
+    let randNum = $state();
+
+    onMount(async () => {
+        const response = await fetch('http://localhost:8000/rand');
+        const data = await response.json();
+        randNum = data.number;
+    })
+</script>
+
+<main>
+   <h1>Welcome to SvelteKit</h1>
+    <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+    <div>
+        {randNum}
+    </div> 
+</main>
+
+
